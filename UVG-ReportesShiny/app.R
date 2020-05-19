0a046ad3dcf800f75c31b0e0e37b7d257b554a96
unknown <dele17822@uvg.edu.gt>
2020-05-18 23:55:11 -0600

    Tabla casos por municipio

--- UVG-ReportesShiny/app.R
+++ UVG-ReportesShiny/app.R
@@ -10,6 +10,8 @@
 library(shiny)
 library(shinydashboard)
 library(RPostgreSQL)
+library(DT)
+library(ECharts2Shiny)
 
 #Coneccion a DB DUMMY
 
@@ -25,6 +27,13 @@ db_password <- 'SDex2020'
 
 con <- dbConnect(RPostgres::Postgres(), dbname = db, host=host_db, port=db_port, user=db_user, password=db_password)
 
+casosPorMunicipio <- dbGetQuery(con, "select b.descripcion as pais,c.descripcion as municipio,a.caso_confirmado,count(a.codigo_solicitud) as cantidad_registrados from solicitud as a inner join pais as b on a.codigo_pais = b.codigo_pais inner join municipio as c on a.codigo_municipio = c.codigo_municipio group by b.descripcion,c.descripcion,a.caso_confirmado order by c.descripcion;")
+
+cantDeSintomas <- dbGetQuery(con, "select b.descripcion ,count(a.codigo_sintoma) from sintomas_persona as a inner join tipo_sintoma as b on a.codigo_sintoma = b.codigo_sintoma group by b.descripcion;")
+
+sexoVsCasos <- dbGetQuery(con, "select case sexo when 'F' then 'Femenino' when 'M' then 'Masculino' end as sexo,caso_confirmado,count(codigo_solicitud) from solicitud group by sexo,caso_confirmado order by sexo;")
+
+print(cantDeSintomas)
 
 ui <- dashboardPage(
     dashboardHeader(title= "Reportes COVID-19 UVG"),
@@ -42,12 +51,11 @@ ui <- dashboardPage(
             tabItem("general",
                     fluidPage(
                         h1("General"),
-                        br(),
-                        box(plotOutput("correlation_plot"), width = 8),
-                        box(selectInput("features", "Features:",
-                                        c("Sepal.Width", "Petal.Length", "Petal.Width")),
-                            width = 4
-                        )
+                        hr(),
+                        h3("Casos por Municipio"),
+                        DT::dataTableOutput("cpm"),
+                        h3("Cantidad de Sintomas"),
+                        tags$div(id="test", style="width:50%;height:400px;"),
                     )
             ),
             tabItem("cases",
@@ -75,9 +83,10 @@ ui <- dashboardPage(
 )
 
 server <- function(input, output){
-    output$correlation_plot <- renderPlot({
-        plot(iris$Sepal.Length, iris[[input$features]], xlab = "Sepal length", ylab = "Features")
+    output$cpm = DT::renderDataTable({
+        casosPorMunicipio
     })
+    renderBarChart(div_id = "test", grid_left = '1%', direction = "horizontal", data = cantDeSintomas)
 }
 
 shinyApp(ui, server)
