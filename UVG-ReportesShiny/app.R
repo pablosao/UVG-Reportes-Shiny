fd97c395e52b72b29cdb7eb6c6fa883da2a313a0
eli17048 <andrea.elias16@gmail.com>
2020-05-31 20:38:33 -0600

    Mapa

--- UVG-ReportesShiny/app.R
+++ UVG-ReportesShiny/app.R
@@ -21,6 +21,39 @@ library(ECharts2Shiny)
 library(ini)
 source("queryManager.R")
 library(plotly)
+library(leaflet)
+library(viridis)
+library(rworldmap)
+library(maps)
+library(ggmap)
+read.csv("~/covid27.csv") -> data
+data$Province.State <- lapply(data$Province.State, as.character)
+
+str(data$Province.State)
+
+data$Country.Region<- lapply(data$Country.Region, as.character)
+
+key = "AIzaSyB8hwaB2EEOsucgdc80NCaGp7p7XiSPBvQ" #please use your own API key, I have input the key previously.
+register_google(key=key)
+
+#obtain the coordinates
+country <- data$Country.Region
+cities <- data$Province.State
+#use lapply to iterate and obtain the coordinates information.
+loc1 <- do.call(rbind, lapply(cities, function(x) geocode(x))) 
+loc1 <- as.data.frame(loc1)
+loc2 <- do.call(rbind, lapply(country, function(x) geocode(x))) 
+loc2 <- as.data.frame(loc2)
+
+#create them as data.frame
+as.data.frame(data[,1:2]) -> data2
+data2$lon <- ifelse(data2[,1]== "", loc2[,1], loc1[,1])
+data2$lat <- ifelse(data2[,1]== "", loc2[,2], loc1[,2])
+data2 <- data.frame(data2, Confirmed=data$Confirmed, Deaths=data$Deaths, Recovered= data$Recovered)
+
+
+
+
 
 # PSAO / 19-05-2020 / Ser cargan datos de conexion configurado en archivo .ini
 datosConexion <- read.ini('conexion.ini')
@@ -127,7 +160,8 @@ ui <- dashboardPage(
             ),
             tabItem("regions",
                     fluidPage(
-                        h1("Regiones")
+                        h1("Regiones"),
+                        plotlyOutput("p", height = "600px")
                     )
             ),
             tabItem("symptoms",
@@ -167,6 +201,56 @@ server <- function(input, output){
     
     output$sexvscases <- renderPlot({
         pie(sexoVsCasos$cantidad, labels = c("Femenino", "Femenino Confirmado", "Masculino", "Masculino Convirmado"), main = "Sexo Vs Casos")
+    })
+    output$p <- renderPlotly({
+        #static map with ggplot
+        world  <- map_data("world")
+        
+        g <- data2 %>%
+            arrange(Confirmed) %>% 
+            mutate( name=factor(Country.Region, unique(Country.Region))) %>% 
+            ggplot() +
+            geom_polygon(data = world, aes(x=long, y = lat, group = group), fill="grey", alpha=0.3) +
+            geom_point( aes(x=lon, y=lat, size=Confirmed, color=Confirmed), alpha=0.4) +
+            scale_size_continuous(range=c(1,10)) +scale_color_viridis (trans="log") +
+            ggtitle("Coronavirus outbreak until
+    27 Jan 2020")  + theme(plot.title =
+                               element_text(size = 10, face =
+                                                "bold"), legend.title =
+                               element_text(size = 15), legend.text
+                           = element_text(size = 10))+
+            coord_map()
+        
+        
+        #interactive map with plotly
+        #mutate the data, so the country/cities can be viewed when you hover the region
+        
+        
+        
+        data2$New.R <- ifelse(data2[,1]== "", data2[,2], data2[,1])
+        #it will replace the city that does not mention in the table with the country information
+        
+        data2n <- data2 %>%
+            arrange(Deaths) %>%
+            mutate(Country.Region=factor(New.R, unique(New.R))) %>%
+            mutate( mytext=paste(
+                "CountryOrCity: ", New.R, "\n", 
+                "Confirmed: ", Deaths, sep=""))
+        
+        
+        #this is actually still static
+        p <- data2n %>%
+            ggplot() +
+            geom_polygon(data = world, aes(x=long, y = lat, group = group), fill="grey", alpha=0.3) +
+            geom_point(aes(x=lon, y=lat, size=Confirmed, color=Confirmed, text=mytext, alpha=0.5) ) +
+            scale_size_continuous(range=c(1,15)) +
+            scale_color_viridis(option="inferno", trans="log" ) +
+            scale_alpha_continuous(trans="log") +
+            theme_void() +
+            coord_map() +
+            theme(legend.position = "none")
+        #to make it as an interactive map    
+        p <- ggplotly(p, tooltip="text")
     })
     
 }
